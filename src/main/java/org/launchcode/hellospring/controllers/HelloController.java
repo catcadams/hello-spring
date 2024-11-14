41d83900cd5eed8ad97a2358ce031b0b087734e4
catcadams <catceciliaadams@gmail.com>
2024-11-13 21:32:07 -0600

    controller exercises

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,6 +4,8 @@ package org.launchcode.hellospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.MessageFormat;
+
 @Controller
 public class HelloController {
 
@@ -21,11 +23,11 @@ public class HelloController {
     }
 
     //create handler that handles requests of the form /hello?name=LaunchCode
-    @RequestMapping(value="hello", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
-    }
+//    @RequestMapping(value="hello", method = {RequestMethod.GET, RequestMethod.POST})
+//    @ResponseBody
+//    public String helloWithQueryParam(@RequestParam String name) {
+//            return "Hello, " + name + "!";
+//    }
 
     //handles request of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
@@ -41,9 +43,46 @@ public class HelloController {
                 "<body>" +
                 "<form action='hello' method='post'>" +
                 "<input type='text' name='name'>" +
+                "<select name='language'>" +
+                "<option value=''> English </option>" +
+                "<option value='french'> French </option>" +
+                "<option value='spanish'> Spanish </option>" +
+                "<option value='greek'> Greek </option>"+
+                "<option value='croatian'> Croatian </option>" +
+                "<option value='lithuanian'> Lithuanian </option>" +
+                "</select>" +
                 "<input type='submit' value='Greet me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
+    }
+
+    public static String createMessage(String name, String language) {
+        String greeting = "";
+        if(language =="french") {
+            greeting = "Bonjour";
+        } else if (language == "spanish") {
+            greeting= "Hola";
+        } else if (language == "greek") {
+            greeting= "Γειά σου";
+        } else if(language == "croatian"){
+            greeting= "Zdravo";
+        } else if (language == "lithuanian") {
+            greeting="Sveiki";
+        } else {
+            greeting= "Hello";
+        }
+
+        return MessageFormat.format("{0}, {1}!", greeting, name);
+    }
+
+    @RequestMapping(value="hello",method = {RequestMethod.POST})
+    @ResponseBody
+    public String helloPost(@RequestParam String name, String language) {
+        if (name=="") {
+            name = "LaunchCode";
+        }
+
+        return createMessage(name, language);
     }
 }
